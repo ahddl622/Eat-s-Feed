35866eb6f8bb129a3dd20ecd323161a155c26903
Seoweol <ahddl622@naver.com>
2024-02-07 20:23:10 +0900

    components/createFeed -> CreateFeed 이름 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-
+import { useEffect } from 'react';
 import { app } from './firebase';
-import React, { useEffect } from 'react';
-
+import Main from 'pages/Main';
 
 function App() {
   useEffect(() => {
     console.log('firebase에서 app 잘 가져와지나요?', app);
   });
-  return <div>App</div>;
+  return (
+    <div>
+      App
+      <Main />
+    </div>
+  );
 }
 
 export default App;
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,14 @@
+import CreateFeed from 'components/CreateFeed';
 import React from 'react';
 
 function Main() {
-  return <div>Main</div>;
+  return (
+    <div>
+      Main
+      <button>새 글 작성하기</button>
+      <CreateFeed />
+    </div>
+  );
 }
 
 export default Main;
