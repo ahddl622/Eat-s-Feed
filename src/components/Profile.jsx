08b746b9b7b93b2371ffd90b6c8bad9f31595287
kimsohyeon <aotoyae@gmail.com>
2024-02-11 20:55:28 +0900

    add: MyPage 구성
    Profile, MyFeed 컴포넌트 추가
    MyPage 레이아웃 구성

---
+++ src/components/Profile.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const StSection = styled.section`
+  padding: 10px;
+
+  border: 2px solid #fff;
+  box-shadow: 3px 5px 12px 5px #ffe5e5;
+  border-radius: 35px;
+`;
+
+function Profile() {
+  return <StSection>Profile</StSection>;
+}
+
+export default Profile;
