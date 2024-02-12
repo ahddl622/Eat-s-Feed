755e5044c957ebccd065cb1435d59022dc43f4e2
kimsohyeon <aotoyae@gmail.com>
2024-02-13 02:06:06 +0900

    Add: auth 테스트

--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -1,5 +1,6 @@
 import MyFeed from 'components/MyFeed';
 import Profile from 'components/Profile';
+import { auth } from '../firebase';
 import styled from 'styled-components';
 
 const StWrap = styled.div`
@@ -28,6 +29,7 @@ const StText = styled.div`
 `;
 
 function Mypage() {
+  console.log(auth.currentUser);
   return (
     <StWrap>
       <StArticle>
